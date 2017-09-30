49c39a20477e619da2e174da17651fdb02813f7e
Izaac Zavaleta <izaac.zavaleta@moduscreate.com>
2017-09-30 13:42:58 -0700

    adding package configuration and app logic

--- server/src/app.js
+++ server/src/app.js
@@ -3,24 +3,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const config = require('./config/config');
 
 const app = express();
+const { sequelize } = require('./models');
+
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-/* GET /status endpoint */
-app.get('/status', (req, res) => {
-  res.send({
-    message: 'hello world',
-  });
-});
+require('./routes')(app);
 
-/* POST /register endpoint */
-app.post('/register', (req, res) => {
-  res.send({
-    message: `Hello ${req.body.email} your user was registered.`,
+sequelize.sync()
+  .then(() => {
+    app.listen(config.port);
+    console.log(`Server started on port ${config.port}`);
   });
-});
-
-app.listen(process.env.PORT || 8081);
