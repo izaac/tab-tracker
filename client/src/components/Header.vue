45d0084684647a65063df5f75afa06d5092d8f4a
Izaac Zavaleta <izaac.zavaleta@moduscreate.com>
2017-10-21 07:58:03 -0700

    Adding Header component and styling to Register form inputs

---
+++ client/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+    v-toolbar(fixed class="cyan")
+      v-toolbar-title(class="mr-4") TabTracker
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.error {
+  color:red;
+}
+</style>
