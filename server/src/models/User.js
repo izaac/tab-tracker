e4423b05f785afa90e187367d7d168954ab61f0c
Izaac Zavaleta <izaac.zavaleta@moduscreate.com>
2017-11-02 13:20:30 -0700

    adding bcrypt integration for password encryption

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,8 +1,34 @@
-module.exports = (sequelize, DataTypes) =>
-  sequelize.define('User', {
+const Promise = require('bluebird');
+const bcrypt = Promise.promisifyAll(require('bcrypt-nodejs'));
+
+function hashPassword(user) {
+  const SALT_PASSWORD = 8;
+  if (!user.changed('password')) {
+    return false;
+  }
+  return bcrypt.genSaltAsync(SALT_PASSWORD)
+    .then(salt => bcrypt.hashAsync(user.password, salt, null))
+    .then((hash) => {
+      user.setDataValue('password', hash);
+    });
+}
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
     email: {
       type: DataTypes.STRING,
       unique: true,
     },
     password: DataTypes.STRING,
+  }, {
+    hooks: {
+      beforeCreate: hashPassword,
+      beforeUpdate: hashPassword,
+      beforeSave: hashPassword,
+    }
   });
+  User.prototype.comparePassword = function(password) {
+    return bcrypt.compareAsync(password, this.password);
+  };
+  return User;
+};
