91af371134b09e07678a048876c884b63e3ca970
Izaac Zavaleta <jorge.izaac@gmail.com>
2017-11-12 10:30:52 -0700

    adding example with static data to list songs

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -2,6 +2,10 @@
   v-layout(column)
     v-flex(xs6 offset-xs3)
       panel(title="Songs")
+        div(v-for="song in songs" :key="song.title").
+          {{song.title}} - 
+          {{song.artist}} - 
+          {{song.album}}
 </template>
 
 <script>
@@ -10,6 +14,22 @@ import Panel from '@/components/Panel';
 export default {
   components: {
     Panel,
+  },
+  data() {
+    return {
+      songs: [
+        {
+          title: 'Float On',
+          artist: 'Modest Mouse',
+          album: 'Good News for People who Loves Bad News',
+        },
+        {
+          title: 'Float On',
+          artist: 'Modest Mouse',
+          album: 'Good News for People who Loves Bad News',
+        },
+      ],
+    };
   },
 };
 </script>
